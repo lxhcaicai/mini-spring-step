82ffb9f02b4edd90e8e3d3d142d178bfa26f7a5c
lxhcaicai <2778763221@qq.com>
2024-08-07 23:10:16 +0800

    init & 最简单的bean容器

---
+++ small-spring-step-01/src/test/java/org/springframework/test/ioc/SimpleBeanContainerTest.java
@@ -0,0 +1,26 @@
+package org.springframework.test.ioc;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.BeanFactory;
+
+public class SimpleBeanContainerTest {
+
+
+    @Test
+    public void testGetBean() {
+        BeanFactory beanFactory = new BeanFactory();
+        beanFactory.registerBean("helloService",new HelloService());
+        HelloService helloService = (HelloService) beanFactory.getBean("helloService");
+        Assertions.assertNotNull(helloService);
+        Assertions.assertSame(helloService.sayHello(), "hello");
+    }
+
+    class HelloService {
+        public String sayHello() {
+            System.out.println("hello");
+            return "hello";
+        }
+    }
+
+}
