0d135c6fc22e38956aeb1421e63f31436ec99864
lxhcaicai <2778763221@qq.com>
2024-08-08 00:00:02 +0800

    step2 BeanDefinition和BeanDefinitionRegistry

---
+++ mini-spring-step-02-bean-definition-and-bean-definition-registry/src/main/java/org/springframework/beans/factory/support/AbstractAutowireCapableBeanFactory.java
@@ -0,0 +1,27 @@
+package org.springframework.beans.factory.support;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanDefinition;
+
+public abstract class AbstractAutowireCapableBeanFactory extends AbstractBeanFactory{
+
+    @Override
+    protected Object createBean(String beanName, BeanDefinition beanDefinition) throws BeansException {
+        return doCreateBean(beanName, beanDefinition);
+    }
+
+    private Object doCreateBean(String beanName, BeanDefinition beanDefinition) {
+        Class beanClass = beanDefinition.getBeanClass();
+        Object bean = null;
+        try {
+            bean = beanClass.newInstance();
+        } catch (Exception exception) {
+            throw new BeansException("Instantiation of bean failed",exception);
+        }
+
+        addSingleton(beanName, bean);
+        return bean;
+    }
+
+
+}
